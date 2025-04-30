6e19ed76ac51d8f0e064fcaf3ae9925ab3905c0e
Connor McGartland <cmcgartl@usc.edu>
2025-04-30 01:47:33 -0700

    Fix: Solved issue where face-api models loaded properly locally but not in production

--- src/FaceDetection.tsx
+++ src/FaceDetection.tsx
@@ -1,6 +1,6 @@
 import * as faceapi from 'face-api.js';
 
-const MODEL_URL = process.env.PUBLIC_URL + '/models';
+const MODEL_URL = '/models';
 
 export const loadModels = async () => {
     console.log("loadModels() called");
